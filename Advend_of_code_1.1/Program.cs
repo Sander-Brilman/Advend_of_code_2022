2f1e77e9d98c7d89730523ba15217cffd37b197f
Sander-Brilman <brilmansander@gmail.com>
2022-12-01 10:18:52 +0100

    Add project files.

---
+++ Advend_of_code_1.1/Program.cs
@@ -0,0 +1,43 @@
+﻿string puzzle = "";
+string output = "";
+do
+{
+    Console.WriteLine("Choose the puzzle (1, 2)");
+    Console.ReadLine();
+    Console.Clear();
+} while(puzzle != "1" && puzzle != "2");
+
+
+
+
+StreamReader sr = new($"../../../input{puzzle}.txt");
+
+
+if (puzzle == "1")
+{
+    string line;
+    int index = 0;
+    List<int> calories = new() {0};
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        if (line == "")
+        {
+            index++;
+            calories.Add(0);
+            continue;
+        }
+
+        calories[index] += int.Parse(line);
+    }
+
+    output = calories.Max().ToString();
+} else
+{
+
+}
+
+
+
+Console.WriteLine($"Output puzzle {puzzle}: {output}");
+Console.ReadLine();
