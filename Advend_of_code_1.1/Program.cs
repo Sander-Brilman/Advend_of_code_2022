ad09a6eab7ac809985b87c9b6f55667000e5f4bf
Sander-Brilman <brilmansander@gmail.com>
2022-12-02 00:32:58 +0100

    finished day 1

--- Advend_of_code_1.1/Program.cs
+++ Advend_of_code_1.1/Program.cs
@@ -1,40 +1,42 @@
-﻿string puzzle = "";
-string output = "";
+﻿string puzzle;
+string output;
 do
 {
     Console.WriteLine("Choose the puzzle (1, 2)");
-    Console.ReadLine();
+    puzzle = Console.ReadLine();
     Console.Clear();
 } while(puzzle != "1" && puzzle != "2");
 
 
 
 
-StreamReader sr = new($"../../../input{puzzle}.txt");
+StreamReader sr = new($"../../../input.txt");
 
+string line;
+int index = 0;
+List<int> calories = new() { 0 };
+
+while ((line = sr.ReadLine()) != null)
+{
+    if (line == "")
+    {
+        index++;
+        calories.Add(0);
+        continue;
+    }
+
+    calories[index] += int.Parse(line);
+}
 
 if (puzzle == "1")
 {
-    string line;
-    int index = 0;
-    List<int> calories = new() {0};
-
-    while ((line = sr.ReadLine()) != null)
-    {
-        if (line == "")
-        {
-            index++;
-            calories.Add(0);
-            continue;
-        }
-
-        calories[index] += int.Parse(line);
-    }
-
     output = calories.Max().ToString();
 } else
 {
+    calories.Sort();
+    calories.Reverse();
 
+    output = (calories[0] + calories[1] + calories[2]).ToString();
 }
 
 
