6a7cb289180aa7e2b5c37a1b1ddb39baeb2e65ae
Sander-Brilman <brilmansander@gmail.com>
2022-12-03 14:40:24 +0100

    working on day 3

---
+++ Advend_of_code_1.1/Puzzles/Day_3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advend_of_code_1._1.Puzzles
+{
+    internal class Day_3 : PuzzleSolution
+    {
+        public Day_3(StreamReader file) : base(file) { }
+
+        private static readonly List<char> Alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray().ToList();
+
+        public override string Puzzle1()
+        {
+            string line;
+            while ((line = InputFile.ReadLine()) != null)
+            {
+                int middle = line.Length / 2;
+                string part1 = line[..middle];
+                string part2 = line[middle..];
+            }
+        }
+
+        public override string Puzzle2()
+        {
+            return base.Puzzle2();
+        }
+
+        private int 
+    }
+}
