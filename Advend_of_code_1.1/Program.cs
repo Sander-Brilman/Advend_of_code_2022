94539fcb3a5182329daa0d5382ad6633b3bec459
Sander Brilman <0339663@student.rocvantwente.nl>
2022-12-03 20:21:37 +0100

    finished day 3

--- Advend_of_code_1.1/Program.cs
+++ Advend_of_code_1.1/Program.cs
@@ -43,6 +43,7 @@ bool runApp()
     {
         1 => new Day_1(inputFile),
         2 => new Day_2(inputFile),
+        3 => new Day_3(inputFile),
         _ => new Day_1(inputFile),
     };
 
--- Advend_of_code_1.1/Puzzles/Day_3.cs
+++ Advend_of_code_1.1/Puzzles/Day_3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,20 +15,84 @@ namespace Advend_of_code_1._1.Puzzles
 
         public override string Puzzle1()
         {
+            int total = 0;
             string line;
             while ((line = InputFile.ReadLine()) != null)
             {
                 int middle = line.Length / 2;
                 string part1 = line[..middle];
                 string part2 = line[middle..];
+
+                List<char> part1Chars = part1.ToCharArray().ToList();
+
+                foreach (char part in part1Chars)
+                {
+                    if (!part2.Contains(part))
+                    {
+                        continue;
+                    }
+
+                    total += GetLetterScore(part);
+                    break;
+                }
             }
+
+            return total.ToString();
         }
 
         public override string Puzzle2()
         {
-            return base.Puzzle2();
+            int total = 0;
+
+            string line;
+            List<string> lines = new();
+            while ((line = InputFile.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            for (int index = 0; index < lines.Count; index += 3)
+            {
+                List<string> backpacks = new()
+                {
+                    lines[index],
+                    lines[index + 1],
+                    lines[index + 2]
+                };
+                backpacks = backpacks.OrderBy(x => x.Length).ToList();
+
+                List<char> commonChars = backpacks[0].ToCharArray().ToList();
+                backpacks.RemoveAt(0);
+
+                foreach(string backpack in backpacks)
+                {
+                    int length = commonChars.Count;
+                    for (int commonCharindex = length - 1; commonCharindex >= 0; commonCharindex--)
+                    {
+                        char commonChar = commonChars[commonCharindex];
+                        if (backpack.Contains(commonChar))
+                        {
+                            continue;
+                        }
+
+                        commonChars.Remove(commonChar);
+                    }
+
+
+                }
+
+                total += GetLetterScore(commonChars[0]);
+            }
+
+            return total.ToString();
+
         }
 
-        private int 
+        private static int GetLetterScore(char letter)
+        {
+            int score = Alphabet.IndexOf(char.ToLower(letter)) + 1;
+            score += char.IsUpper(letter) ? 26 : 0;
+            return score;
+        }
     }
 }
