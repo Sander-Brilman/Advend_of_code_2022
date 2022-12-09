139e074451923d5f9521522b8e8bbee4bb93cc21
Sander-Brilman <brilmansander@gmail.com>
2022-12-09 07:59:08 +0100

    working on day 9

---
+++ Advend_of_code_1.1/Puzzles/Day_9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advend_of_code_1._1.Puzzles
+{
+    internal class Day_9 : PuzzleSolution
+    {
+        public Day_9(StreamReader file) : base(file) { }
+
+        public override string Puzzle1()
+        {
+            string line;
+            while ((line = InputFile.ReadLine()) != null)
+            {
+
+            }
+
+            return "oeleh";
+        }
+
+        public override string Puzzle2()
+        {
+            return base.Puzzle2();
+        }
+    }
+}
