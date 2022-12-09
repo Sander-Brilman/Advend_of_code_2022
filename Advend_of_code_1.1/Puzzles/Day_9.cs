a04aeae0b84224822ce57fb2c43869f0cafb9aa4
Sander Brilman <0339663@student.rocvantwente.nl>
2022-12-10 00:25:46 +0100

    finished part 1

--- Advend_of_code_1.1/Puzzles/Day_9.cs
+++ Advend_of_code_1.1/Puzzles/Day_9.cs
@@ -10,24 +10,31 @@ namespace Advend_of_code_1._1.Puzzles
     {
         public Day_9(StreamReader file) : base(file) { }
 
+        private static readonly bool _map = false;
+
         private int _headLocationX = 0;
-        private int _headLocationY = 0;
+        private int _headLocationY = 10;
 
         private int _tailLocationX = 0;
-        private int _tailLocationY = 0;
+        private int _tailLocationY = 10;
 
-        private HashSet<string> _tailLocaions = new() { "00" };
+        private HashSet<string> _tailLocaions = new();
 
         public override string Puzzle1()
         {
+            PrintMap();
+            SaveLocation();
             string line;
             while ((line = InputFile.ReadLine()) != null)
             {
                 string[] movementInfo = line.Split(' ');
                 MoveHead(int.Parse(movementInfo[1]), movementInfo[0][0]);
+
+
+
             }
 
-            return "oeleh";
+            return _tailLocaions.Count.ToString();
         }
 
         public override string Puzzle2()
@@ -50,29 +57,88 @@ namespace Advend_of_code_1._1.Puzzles
                         break;
 
                     case 'U':// up
-                        _headLocationY++;
+                        _headLocationY--;
                         break;
 
                     case 'D':// down
-                        _headLocationY--;
+                        _headLocationY++;
                         break;
                 }
                 MoveTail();
+
+                if (_map)
+                {
+                    Console.WriteLine($"{directon} {amount} ({i + 1}/{amount}) - {_tailLocaions.Count} tail places");
+                    Console.WriteLine();
+                }
+
+                PrintMap();
             }
         }
 
         private void MoveTail()
         {
             int distanceX = _headLocationX - _tailLocationX;
-            int distanceY = _headLocationY - _tailLocationY;
+            int absDistanceX = Math.Abs(distanceX);
 
-            if (Math.Abs(distanceX) > 1 || Math.Abs(distanceY) > 1)
+            int distanceY = _headLocationY - _tailLocationY;
+            int absDistanceY = Math.Abs(distanceY);
+
+            if (absDistanceX + absDistanceY == 3)
             {
-                _tailLocationX += distanceX == 0 ? 0 : distanceX - 1;
-                _tailLocationY += distanceY == 0 ? 0 : distanceY - 1;
+                int increaseX = distanceX + (absDistanceX == 1 ? 0 : GetInt(distanceX));
+                int increaseY = distanceY + (absDistanceY == 1 ? 0 : GetInt(distanceY));
+                _tailLocationX += increaseX;
+                _tailLocationY += increaseY;
+            }
+            else if (absDistanceX > 1 || absDistanceY > 1)
+            {
+                int increaseX = distanceX + GetInt(distanceX);
+                int increaseY = distanceY + GetInt(distanceY);
+                _tailLocationX += distanceX == 0 ? 0 : increaseX;
+                _tailLocationY += distanceY == 0 ? 0 : increaseY;
+            }
+            SaveLocation();
+        }
+
+        private void SaveLocation()
+        {
+            _tailLocaions.Add($"{_tailLocationX} {_tailLocationY}");
+        }
+
+        private void PrintMap()
+        {
+            if (!_map)
+            {
+                return;
             }
 
-            _tailLocaions.Add($"{_tailLocationX}{_tailLocationY}");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write($"{i:D3} ");
+                for (int j = -10; j < 10; j++)
+                {
+                    char character = '.';
+                    if (i == _headLocationY && j == _headLocationX)
+                    {
+                        character = 'H';
+                    }
+                    else if (i == _tailLocationY && j == _tailLocationX)
+                    {
+                        character = 'T';
+                    }
+                    Console.Write(character);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private int GetInt(int input)
+        {
+            return input < 0 ? 1 : -1;
         }
     }
 }
