6f93c215e1efe25cfa57315755ea21b321a00330
sander brilman <brilmansander@gmail.com>
2023-12-05 23:29:14 +0100

    well atleast i tried

--- Puzzels2023/Solutions/Solution5.cs
+++ Puzzels2023/Solutions/Solution5.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Puzzels2023.Solutions;
 public class Solution5(string path) : SolutionBase(path)
 {
+    private class Source
+    {
+        public long Start { get; set; }
+
+        public long RangeLength { get; set; }
+
+        public bool IsDoneForCurrentMap { get; set; } = false;
+
+        public long End => Start + (RangeLength - 1);
+    }
+
     private static long[] GetNumbersFromString(string str)
     {
         return str
@@ -80,6 +92,7 @@ public class Solution5(string path) : SolutionBase(path)
         return seed;
     }
 
+
     public override string GetFirstSolution()
     {
         long[] source = GetFirstLineNumbers();
@@ -106,25 +119,143 @@ public class Solution5(string path) : SolutionBase(path)
             long seedStart = seedRanges[i];
             long seedRange = seedRanges[i + 1];
 
-            Parallel.For(
-                seedStart,
-                seedStart + seedRange,
-                new ParallelOptions()
-                {
-                    MaxDegreeOfParallelism = 150
-                },
-                seed =>
-                {
-                    long minOfSelection = GetLowestLocationForSeed(seed);
 
-                    minSoFar = minSoFar == null
-                        ? minOfSelection
-                        : (minSoFar > minOfSelection ? minOfSelection : minSoFar);
-                });
+            List<Source> sources = [new Source()
+            {
+                RangeLength = seedRange,
+                Start = seedStart,
+            }];
 
-            Console.WriteLine($"[{DateTime.UtcNow}] complete {i} / {forLength}");
+            var maps = GetMaps();
+            foreach (var map in maps)
+            {
+                foreach (Source s in sources)
+                    s.IsDoneForCurrentMap = false;
+
+                foreach (var mapRule in map)
+                {
+
+                    for (int sourceIndex = 0; sourceIndex < sources.Count; sourceIndex++)
+                    {
+                        var source = sources[sourceIndex];
+                        if (source.IsDoneForCurrentMap) { continue; }
+
+
+                        long[] numbers = GetNumbersFromString(mapRule);
+
+                        long mapDesinationStart = numbers[0];
+                        long mapSourceStart = numbers[1];
+                        long mapRangeLength = numbers[2];
+                        long mapSourceEnd = mapSourceStart + (mapRangeLength - 1);
+
+                        if (source.End < mapSourceStart || source.Start > mapSourceEnd)
+                        {
+                            //the whole set is completely ignored, continue to next
+                            continue;
+                        }
+                        else if (source.Start >= mapSourceStart && source.End <= mapSourceEnd)
+                        {
+                            // the whole set can be incremented as a whole, continue to next set
+                            source.Start += (mapDesinationStart - mapSourceStart);
+                            source.IsDoneForCurrentMap = true;
+                            continue;
+                        }
+                        else if (source.Start < mapSourceStart && source.End > mapSourceEnd)
+                        {
+                            // create source on the front
+                            long newSetLength = mapSourceStart - source.Start;
+                            Source newSplitSourceLeft = new()
+                            {
+                                Start = source.Start,
+                                RangeLength = newSetLength,
+                            };
+
+                            sources.Add(newSplitSourceLeft);
+
+
+                            // create source on the back
+                            newSetLength = source.End - mapSourceEnd;
+                            Source newSplitSourceRight = new()
+                            {
+                                Start = mapSourceEnd + 1,
+                                RangeLength = newSetLength,
+                            };
+
+                            sources.Add(newSplitSourceRight);
+
+
+
+                            // update current source
+                            source.Start = mapSourceStart;
+                            source.RangeLength = mapRangeLength;
+
+                            source.Start += (mapDesinationStart - mapSourceStart);
+                            source.IsDoneForCurrentMap = true;
+
+                            continue;
+                        }
+                        else if (source.Start < mapSourceStart)
+                        {
+                            // split on mapSourceStart
+
+                            long newSetLength = mapSourceStart - source.Start;
+
+                            Source newSplitSource = new()
+                            {
+                                Start = source.Start,
+                                RangeLength = newSetLength,
+                            };
+
+                            sources.Add(newSplitSource);
+
+                            source.Start = mapSourceStart;
+                            source.Start += (mapDesinationStart - mapSourceStart);
+
+                            source.RangeLength -= newSetLength;
+                            source.IsDoneForCurrentMap = true;
+
+                            continue;
+                        }
+                        else if (source.End < mapSourceEnd)
+                        {
+                            // split on mapSourceEnd
+
+                            long newSetLength = source.End - mapSourceEnd;
+
+                            Source newSplitSource = new()
+                            {
+                                Start = mapSourceEnd + 1,
+                                RangeLength = newSetLength,
+                            };
+
+                            sources.Add(newSplitSource);
+
+                            source.Start += (mapDesinationStart - mapSourceStart);
+                            source.RangeLength -= newSetLength;
+                            source.IsDoneForCurrentMap = true;
+
+                            continue;
+                        }
+                    }// source foreach
+
+                }// mapRule foreach
+
+            }// maps foreach
+
+
+            long minFromTheseSets = sources
+                .MinBy(x => x.Start)
+                !.Start;
+
+            if (minSoFar is null)
+            {
+                minSoFar = minFromTheseSets;
+                continue;
+            }
+
+            minSoFar = minFromTheseSets < minSoFar
+                ? minFromTheseSets : minSoFar;
         }
-
 
         return minSoFar!.ToString()!;
     }
